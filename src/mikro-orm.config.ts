ec76a1518aa076165cf62d8e78a888be0aaec23d
Clément Nerma <clement.nerma@gmail.com>
2022-03-06 11:57:34 +0100

    Use ts-morph for reflection in MikroORM

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,9 +1,11 @@
 import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs'
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 import { CONFIG } from './config'
 
 const options: MikroOrmModuleSyncOptions = {
   entities: ['./dist/**/*.entity.js'],
   entitiesTs: ['./src/**/*.entity.ts'],
+  metadataProvider: TsMorphMetadataProvider,
   type: 'postgresql',
   host: CONFIG.DB_HOST,
   port: CONFIG.DB_PORT,
