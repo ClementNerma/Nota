539c3c854701562ac9527d19605aba8c26cdbd3f
Clément Nerma <clement.nerma@gmail.com>
2022-03-09 12:54:14 +0100

    Add support for external messages sending

--- src/modules/message/message.service.ts
+++ src/modules/message/message.service.ts
@@ -1,7 +1,7 @@
 import { EntityRepository } from '@mikro-orm/core'
 import { InjectRepository } from '@mikro-orm/nestjs'
 import { Injectable } from '@nestjs/common'
-import { ForbiddenError } from 'apollo-server-express'
+import { ApolloError, ForbiddenError } from 'apollo-server-express'
 import { Correspondent } from '../correspondent/correspondent.entity'
 import { CorrespondentGuard } from '../correspondent/correspondent.guard'
 import { Viewer } from '../graphql/auth'
@@ -10,6 +10,9 @@ import { MessageSendToExternalInputDTO } from './dtos/message-send-to-external.i
 import { MessageSendInputDTO } from './dtos/message-send.input'
 import { MessageSentDTO } from './dtos/message-sent.dto'
 import { Message, MessageDirection } from './message.entity'
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client/core'
+import { fetch } from 'cross-fetch'
+import { API_KEY_HEADER } from '../graphql/external'
 
 @Injectable()
 export class MessageService {
@@ -37,7 +40,42 @@ export class MessageService {
   async sendMessageToExternal(from: Viewer, input: MessageSendToExternalInputDTO): Promise<MessageSentDTO> {
     const correspondent = await this.correspondentGuard.getViewerCorrespondent(from, input.correspondentId)
 
-    throw new Error('// TODO after design')
+    if (!correspondent.userPermissions.canSendMessages) {
+      throw new ForbiddenError('This correspondent did not allow messages sending from your side')
+    }
+
+    const client = new ApolloClient({
+      uri: correspondent.serverUrl,
+      cache: new InMemoryCache(),
+      link: new HttpLink({
+        uri: correspondent.serverUrl,
+        fetch,
+        headers: {
+          [API_KEY_HEADER]: correspondent.userApiKey,
+        },
+      }),
+    })
+
+    const res = await client.mutate<{ sendMessage: MessageSentDTO }, { input: MessageSendInputDTO }>({
+      mutation: gql`
+        mutation ($input: MessageSendInputDTO!) {
+          sendMessage(input: $input) {
+            messageId
+          }
+        }
+      `,
+      variables: {
+        input: {
+          encryptedData: input.encryptedData,
+        },
+      },
+    })
+
+    if (!res.data) {
+      throw new ApolloError('Server responded successfully but without any data')
+    }
+
+    return { messageId: res.data.sendMessage.messageId }
   }
 
   async getMessagesOf(user: User): Promise<Message[]> {
