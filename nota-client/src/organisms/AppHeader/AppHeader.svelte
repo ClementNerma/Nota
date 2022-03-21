a9b951a14942e135a711bb0a0c43eb9fe6c78bb8
Clément Nerma <clement.nerma@gmail.com>
2022-03-21 17:40:13 +0100

    Add button to add another account

--- nota-client/src/organisms/AppHeader/AppHeader.svelte
+++ nota-client/src/organisms/AppHeader/AppHeader.svelte
@@ -1,6 +1,8 @@
 <script type="ts">
-  import { Link } from 'svelte-navigator'
+  import { Link, useNavigate } from 'svelte-navigator'
   import { authData, isAuth, logout, profiles, switchToProfile } from '../../others/auth'
+
+  const navigate = useNavigate()
 
   let selectedProfile = $authData?.viewer.uuid
 
@@ -12,6 +14,7 @@
 <div id="profiles">
   {#if $profiles.length > 0}
     <span>Switch to profile:</span>
+
     <select bind:value={selectedProfile} on:change={() => selectedProfile && switchToProfile(selectedProfile)}>
       {#each $profiles as profile}
         <option value={profile.uuid}>{profile.lastDecryptedName}</option>
@@ -24,6 +27,8 @@
     {#if $isAuth}
       <button on:click={() => logout()}>Logout</button>
     {/if}
+
+    <button on:click={() => navigate('/login')}>Add an account</button>
   {:else}
     <p>No available profile, please <Link to="/login">login</Link>.</p>
   {/if}
